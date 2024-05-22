b79da5e0391e9c1198e3343364db703001082589
Sharmin2622 <islam.sharmin111@gmail.com>
2024-05-22 17:09:58 +0600

    added testimonials section

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -29,6 +29,9 @@ const PopularMenu = () => {
                     ></MenuItem>)
                 }
             </div>
+            <div className="text-center">
+                <button className="btn btn-outline border-0 border-b-4 mt-4">View Full Menu</button>
+            </div>
         </section>
     );
 };
