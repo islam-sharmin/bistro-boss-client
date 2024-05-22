1bad627d2bf3e42d626e68a5f5faabc1915cd36e
Sharmin2622 <islam.sharmin111@gmail.com>
2024-05-22 16:14:36 +0600

    added popular menu section

---
+++ src/Shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,19 @@
+
+
+const MenuItem = ({item}) => {
+
+    const {name, image, price, recipe} = item;
+
+    return (
+        <div className="flex space-x-4">
+            <img style={{borderRadius: '0 200px 200px 200px'}} className="bg-yellow-500 w-[100px]" src={image} alt="" />
+            <div>
+                <h3 className="uppercase">{name}-----------</h3>
+                <p>{recipe}</p>
+            </div>
+            <p className="text-yellow-500">{price}</p>
+        </div>
+    );
+};
+
+export default MenuItem;
