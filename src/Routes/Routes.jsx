758fb5e379508ef0175ca94a8e7f02ed475329f4
Sharmin2622 <islam.sharmin111@gmail.com>
2024-05-29 20:11:16 +0600

    added addItem

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,6 @@
 import {
-    createBrowserRouter
-  } from "react-router-dom";
+  createBrowserRouter
+} from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Menu from "../pages/Menu/Menu/Menu";
@@ -12,53 +12,60 @@ import Secret from "../Shared/Secret/Secret";
 import Dashboard from "../Layout/Dashboard";
 import Cart from "../pages/Dashboard/Cart/Cart";
 import AllUsers from "../pages/Dashboard/AllUsers/AllUsers";
+import AddItems from "../pages/Dashboard/AddItems/AddItems";
+import AdminRoute from "./AdminRoute";
 
-  
- export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children: [
-        {
-            path: '/',
-            element: <Home></Home>
-        },
-        {
-          path: 'menu',
-          element: <Menu></Menu>
-        },
-        {
-          path: 'order/:category',
-          element: <Order></Order>
-        },
-        {
-          path: 'login',
-          element: <Login></Login>
-        },
-        {
-          path: 'signup',
-          element: <SignUp></SignUp>
-        },
-        {
-          path: 'secret',
-          element: <PrivateRoute><Secret></Secret></PrivateRoute>
-        }
-      ]
-    },
-    {
-      path: 'dashboard',
-      element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>,
-      children: [
-        {
-          path: 'cart',
-          element: <Cart></Cart>
-        },
 
-        // admin routes
-        {
-          path: 'users',
-          element: <AllUsers></AllUsers>
-        }
-      ]
-    }
-  ]);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: 'menu',
+        element: <Menu></Menu>
+      },
+      {
+        path: 'order/:category',
+        element: <Order></Order>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      },
+      {
+        path: 'signup',
+        element: <SignUp></SignUp>
+      },
+      {
+        path: 'secret',
+        element: <PrivateRoute><Secret></Secret></PrivateRoute>
+      }
+    ]
+  },
+  {
+    path: 'dashboard',
+    element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>,
+    children: [
+      // normal user routes
+      {
+        path: 'cart',
+        element: <Cart></Cart>
+      },
+
+      // admin only routes
+      {
+        path: 'addItems',
+        element: <AdminRoute><AddItems></AddItems></AdminRoute>
+      },
+      {
+        path: 'users',
+        element: <AdminRoute><AllUsers></AllUsers></AdminRoute>
+      }
+    ]
+  }
+]);
---
+++ src/pages/Dashboard/AddItems/AddItems.jsx
@@ -0,0 +1,82 @@
+import { useForm } from "react-hook-form";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { FaUtensils } from "react-icons/fa";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
+
+const AddItems = () => {
+
+    const { register, handleSubmit } = useForm();
+    const axiosPublic = useAxiosPublic();
+    
+    const onSubmit = async(data) => {
+        console.log(data);
+        // image upload t imgbb and then get an url
+        const imageFile = {image: data.image[0]}
+        const res = await axiosPublic.post(image_hosting_api, imageFile, {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        });
+        console.log(res.data);
+    }
+
+
+    return (
+        <div>
+            <SectionTitle heading="add an item" subHeading="What's new?"></SectionTitle>
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <label className="form-control w-full my-4">
+                        <div className="label">
+                            <span className="label-text">Recipe Name*</span>
+                        </div>
+                        <input {...register("name", {required: true})} type="text" placeholder="Recipe Name" className="input input-bordered w-full" />
+                    </label>
+                    <div className="flex gap-6">
+                        {/* category */}
+                        <label className="form-control w-full my-4">
+                            <div className="label">
+                                <span className="label-text">Category*</span>
+                            </div>
+                            <select defaultValue="default" {...register("category", {required: true})}
+                                className="select select-bordered w-full">
+                                <option disabled value="default">Select a category</option>
+                                <option value="salad">Salad</option>
+                                <option value="pizza">Pizza</option>
+                                <option value="soup">Soup</option>
+                                <option value="dessert">Dessert</option>
+                                <option value="drinks">Drinks</option>
+                            </select>
+                        </label>
+
+                        {/* price */}
+                        <label className="form-control w-full my-4">
+                            <div className="label">
+                                <span className="label-text">Price*</span>
+                            </div>
+                            <input {...register("price", {required: true})} type="number" placeholder="Price" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    {/* recipe details */}
+                    <label className="form-control">
+                        <div className="label">
+                            <span className="label-text">Recipe Details*</span>
+                        </div>
+                        <textarea {...register("recipe", {required: true})} className="textarea textarea-bordered h-24" placeholder="Recipe details"></textarea>
+                    </label>
+                    <div className="form-control w-full my-4">
+                    <input {...register("image", {required: true})} type="file" className="file-input w-full max-w-xs" />
+                    </div>
+                    <button className="btn">
+                        Add Item <FaUtensils/>
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default AddItems;
